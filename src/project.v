94fbc521b51466d54f4e8218ad893ca0121aa5a0
JonathanEC1105 <jonathan.cortez0924@gmail.com>
2025-03-07 10:44:40 -0800

    Update project.v

--- src/project.v
+++ src/project.v
@@ -7,7 +7,7 @@
 
 
 
-module tt_um_vga_example(
+module tt_um_JonathanCortez_hdl
 input wire [7:0] ui_in, // Dedicated inputs
 output wire [7:0] uo_out, // Dedicated outputs
 input wire [7:0] uio_in, // IOs: Input path
